e844de0bb7361244e1c3b3f052d0ec0483b20b60
山崎光洋 <yamazakimitsuhiro@yamazakimitsuhironoMacBook-Air.local>
2020-05-31 14:25:42 -0700

    fix

---
+++ js/slideshow.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#example, body').vegas({
+      slides: [
+       { src: '../img/home1.jpg' },
+       { src: '../img/home2.jpg' },
+       { src: '../img/home3.jpg' },
+    //    { src: 'img/slide4.jpg' }
+      ]
+    });
+  });
